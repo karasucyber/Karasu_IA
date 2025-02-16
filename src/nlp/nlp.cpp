4b14fd5c99dd8ad2a13f2833a7b30ea28fa768e6
karasucyber <aquillae777@gmail.com>
2025-02-16 10:46:30 -0300

    teste

--- src/nlp/nlp.cpp
+++ src/nlp/nlp.cpp
@@ -5,6 +5,5 @@
     std::string NLP::processInput(const std::string& input) {
     std::string processedInput = input;
     std::transform(processedInput.begin(), processedInput.end(), processedInput.begin(), ::tolower);
-
     return processedInput;
 }
